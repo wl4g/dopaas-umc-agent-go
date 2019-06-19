2df834cf21549cff69bc81ee58333dfb28a0fa72
983708408@qq.com <983708408@qq.com>
2019-06-19 21:36:30 +0800

    optimize struct

---
+++ go.mod
@@ -0,0 +1,15 @@
+module umc-agent
+
+go 1.12
+
+require (
+	github.com/Shopify/sarama v1.22.1
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/shirou/gopsutil v2.18.12+incompatible
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.2
+)
---
+++ pkg/common_utils.go
@@ -0,0 +1,103 @@
+//
+// Copyright 2017 ~ 2025 the original author or authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var commandPath = "./netCommand.txt"
+var command string
+
+//var sumCommand = "ss -n sport == 22|awk '{sumup += $3};{sumdo += $4};END {print sumup,sumdo}'"
+
+func getNet(port string) string {
+	if command == "" {
+		command = ReadAll(commandPath)
+	}
+	command2 := strings.Replace(command, "#{port}", port, -1)
+	s, _ := exec_shell(command2)
+	fmt.Println(s)
+	return s
+}
+
+//阻塞式的执行外部shell命令的函数,等待执行完毕并返回标准输出
+func exec_shell(s string) (string, error) {
+	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
+	cmd := exec.Command("/bin/bash", "-c", s)
+	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
+	err := cmd.Run()
+	checkErr(err)
+	return out.String(), err
+}
+
+func getDocker() []DockerInfo {
+	var command string = "docker stats --no-stream --format \"{\\\"containerId\\\":\\\"{{.ID}}\\\",\\\"name\\\":\\\"{{.Name}}\\\",\\\"cpuPerc\\\":\\\"{{.CPUPerc}}\\\",\\\"memUsage\\\":\\\"{{.MemUsage}}\\\",\\\"memPerc\\\":\\\"{{.MemPerc}}\\\",\\\"netIO\\\":\\\"{{.NetIO}}\\\",\\\"blockIO\\\":\\\"{{.BlockIO}}\\\",\\\"PIDs\\\":\\\"{{.PIDs}}\\\"}\""
+	s, _ := exec_shell(command)
+	var dockerInfos []DockerInfo
+	if s != "" {
+		s = strings.ReplaceAll(s, "\n", ",")
+		s = strings.TrimSuffix(s, ",")
+		s = "[" + s + "]"
+		json.Unmarshal([]byte(s), &dockerInfos)
+	}
+	return dockerInfos
+}
+
+type DockerInfo struct {
+	ContainerId string `json:"containerId"`
+	Name        string `json:"name"`
+	CpuPerc     string `json:"cpuPerc"`
+	MemUsage    string `json:"memUsage"`
+	MemPerc     string `json:"memPerc"`
+	NetIO       string `json:"netIO"`
+	BlockIO     string `json:"blockIO"`
+	PIDs        string `json:"PIDs"`
+}
+
+//错误处理函数
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+	}
+}
+
+func ReadAll(filePth string) string {
+	f, err := os.Open(filePth)
+	if err != nil {
+		panic(err)
+	}
+	s, _ := ioutil.ReadAll(f)
+	return string(s)
+}
+
+func toJsonString(v interface{}) string {
+	s, err := json.Marshal(v)
+	if err != nil {
+		fmt.Printf("Marshal data error:%v\n", err)
+	}
+	return string(s)
+}
