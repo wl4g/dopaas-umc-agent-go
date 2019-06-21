012169de479e627bc64ca493c7c16cd75c783569
983708408@qq.com <983708408@qq.com>
2019-06-21 15:16:53 +0800

    optimize's2w

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -20,6 +20,7 @@ import (
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
 	"os"
+	"time"
 	"umc-agent/pkg/config"
 )
 
@@ -84,11 +85,12 @@ func createZapCore(filePath string, level zapcore.Level, maxSize int, maxBackups
 		StacktraceKey:  "stacktrace",
 		LineEnding:     zapcore.DefaultLineEnding,
 		EncodeLevel:    zapcore.LowercaseLevelEncoder,  // 小写编码器
-		EncodeTime:     zapcore.ISO8601TimeEncoder,     // ISO8601 UTC 时间格式
+		EncodeTime:     timeEncoder,                    // ISO8601 UTC 时间格式
 		EncodeDuration: zapcore.SecondsDurationEncoder, //
 		EncodeCaller:   zapcore.ShortCallerEncoder,     // (全/短)路径编码器
 		EncodeName:     zapcore.FullNameEncoder,
 	}
+
 	return zapcore.NewCore(
 		zapcore.NewJSONEncoder(encoderConfig),                                           // 编码器配置
 		zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(&hook)), // 打印到控制台和文件
@@ -116,4 +118,9 @@ func parseLogLevel(text string) zapcore.Level {
 	default:
 		return zapcore.InfoLevel
 	}
+}
+
+// Output time encoder
+func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("06-01-02 15:04:05"))
 }
